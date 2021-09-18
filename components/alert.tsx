bb747c44187ae2f9da920dcc2ef61e11d0c77080
Tyler Mills <tmills9208@conestogac.on.ca>
2021-09-17 23:45:03 -0400

    _

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,6 +1,4 @@
 import styles from './alert.module.css'
-// if "Alert".module.css exists, you can use it directly with the following import
-// Need to fix for ts
 import cn from 'classnames'
 import React from 'react'
 
